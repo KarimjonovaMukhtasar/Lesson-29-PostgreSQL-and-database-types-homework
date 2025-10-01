96af0a8f098e05b363485a9fa13e12871d994e7d
KarimjonovaMukhtasar <karimjonovamukhtasar2003@gmail.com>
2025-10-01 18:20:45 +0500

    DOM FRONTEND ALMOST DONE

--- Homework 2 Expenses Tracker via DOM and FS/main.js
+++ Homework 2 Expenses Tracker via DOM and FS/main.js
@@ -1,8 +1,11 @@
+// import fs from "node:fs"
+
 const addButton = document.querySelector(".add_button")
 const addExpense = document.querySelector(".add_expense")
 const cancel = document.querySelector(".cancel")
-const add = document.querySelector(".add")
+const save = document.querySelector(".add")
 const empty = document.querySelector(".empty")
+const search = document.querySelector(".search")
 const expensesList = document.querySelector(".expenses_list")
 const totalSum = document.querySelector(".total_sum")
 const expenseCard = document.querySelector(".expense_card")
@@ -10,37 +13,41 @@ const date = document.querySelector("#created_at")
 const category = document.querySelector("#category")
 const description = document.querySelector("#description")
 const amount = document.querySelector("#amount")
+const deleteEl = document.querySelector(".delete_button")
 
 addButton.addEventListener("click", (e) => {
         e.preventDefault()
         addExpense.style.display = "flex"
+        console.log(addButton)
         empty.style.display = "none"
+        search.style.display = "none"
+        addButton.style.display = "none"
 })
-
 cancel.addEventListener("click", (e) => {
         e.preventDefault()
         addExpense.style.display = "none"
         empty.style.display = "none"
 })
-
-add.addEventListener("click", (e) => {
+save.addEventListener("click", (e) => {
         e.preventDefault()
-        addExpense.style.display = "none"
         empty.style.display = "none"
+        addButton.style.display = "flex"
         expensesList.style.display = "table"
+        addExpense.style.display="none"
         totalSum.style.display = "block"
-        if (date.value && category.value && description.value && amount.value) {
+        if (date.value && category.value && description.value && amount.value){
                 let newRow = document.createElement("tr");
+                totalSum.textContent = Number(amount.value);
                 newRow.innerHTML = `
             <td>${date.value}</td>
             <td>${category.value}</td>
             <td>${description.value}</td>
             <td>${amount.value}</td>
+            <td>${totalSum}</td>
+            <td>${deleteEl}</td>
              `;
                 document.querySelector(".expense_body").appendChild(newRow);
-                totalSum.textContent = Number(totalSum.textContent) + Number(amount.value)
-
         }
+        addExpense.style.display = "none"
 })
-
 let expenses = JSON.parse(localStorage.getItem("expenses")) || [] 
