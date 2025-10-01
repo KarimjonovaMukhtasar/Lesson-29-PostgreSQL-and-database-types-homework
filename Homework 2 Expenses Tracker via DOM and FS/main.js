5b7d6808c42232a941c120b484d10b49663508f9
KarimjonovaMukhtasar <karimjonovamukhtasar2003@gmail.com>
2025-10-01 15:25:07 +0500

    almost ready

--- Homework 2 Expenses Tracker via DOM and FS/main.js
+++ Homework 2 Expenses Tracker via DOM and FS/main.js
@@ -1,5 +1,7 @@
 const addButton = document.querySelector(".add_button")
 const addExpense = document.querySelector(".add_expense")
+const cancel = document.querySelector(".cancel")
+const add = document.querySelector(".add")
 const empty = document.querySelector(".empty")
 const expensesList = document.querySelector(".expenses_list")
 const totalSum = document.querySelector(".total_sum")
@@ -7,14 +9,38 @@ const expenseCard = document.querySelector(".expense_card")
 const date = document.querySelector("#created_at")
 const category = document.querySelector("#category")
 const description = document.querySelector("#description")
-const amount = document.querySelector("amount")
+const amount = document.querySelector("#amount")
 
-addButton.addEventListener("click", ()=>{
+addButton.addEventListener("click", (e) => {
+        e.preventDefault()
         addExpense.style.display = "flex"
-        empty.innerHTML = null
-        expensesList.style.display = "flex"
-        totalSum += amount.value
-        expenseCard.style.display = "flex"
+        empty.style.display = "none"
+})
+
+cancel.addEventListener("click", (e) => {
+        e.preventDefault()
+        addExpense.style.display = "none"
+        empty.style.display = "none"
+})
+
+add.addEventListener("click", (e) => {
+        e.preventDefault()
+        addExpense.style.display = "none"
+        empty.style.display = "none"
+        expensesList.style.display = "table"
+        totalSum.style.display = "block"
+        if (date.value && category.value && description.value && amount.value) {
+                let newRow = document.createElement("tr");
+                newRow.innerHTML = `
+            <td>${date.value}</td>
+            <td>${category.value}</td>
+            <td>${description.value}</td>
+            <td>${amount.value}</td>
+             `;
+                document.querySelector(".expense_body").appendChild(newRow);
+                totalSum.textContent = Number(totalSum.textContent) + Number(amount.value)
+
+        }
 })
 
 let expenses = JSON.parse(localStorage.getItem("expenses")) || [] 
