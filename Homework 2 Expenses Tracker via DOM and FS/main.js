14a713f986cad400a4cdae98074077d355ecf658
KarimjonovaMukhtasar <karimjonovamukhtasar2003@gmail.com>
2025-10-01 12:36:47 +0500

    Done

--- Homework 2 Expenses Tracker via DOM and FS/main.js
+++ Homework 2 Expenses Tracker via DOM and FS/main.js
@@ -1,39 +1,15 @@
+const addButton = document.querySelector(".add_button")
+const addExpense = document.querySelector(".add_expense")
+const empty = document.querySelector(".empty")
+const expensesList = document.querySelector(".expenses_list")
+const totalSum = document.querySelector(".total_sum")
+const expenseCard = document.querySelector(".expense_card")
 
-
-let tasks = JSON.parse(localStorage.getItem("tasks")) || []
-renderTasks(tasks)
-
-.addEventListener("click", () => {
-    let value = inputElement.value
-    if(value !== ""){
-        tasks.push({ id: Date.now(), value })
-    }
-
-    renderTasks(tasks)
-
-    inputElement.value = ""
+addButton.addEventListener("click", ()=>{
+        addExpense.style.display = "flex"
+        addExpense.innerHTML +=`
+        <label id="created_at">Date</label>
+        ` 
 })
 
-.addEventListener("click", (event) => {
-    if(event.target.classList.contains("delete__button")){
-        const parentElement = event.target.closest(".task__card")
-        const id = Number(parentElement.dataset.id)
-
-        tasks = tasks.filter((task) => task.id !== id)
-        renderTasks(tasks)
-    }
-})
-
-
-function renderTasks(tasks) {
-    taskListWrapper.innerHTML = ""
-    tasks.forEach((task) => {
-        taskListWrapper.innerHTML += `
-                <div class="task__card" data-id="${task.id}">
-                    <p>${task.value}</p>
-                    <button class="button"><i class="fa-solid fa-trash delete__button"></i></button>
-                </div>
-        `
-    })
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-}
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [] 
